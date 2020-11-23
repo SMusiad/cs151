b438922070dc463de81d14efeb9c9dddc1ed0798
ReidConsole <jermainereid@mec.science>
2020-11-23 07:59:48 -0500

    Quizzes Update

---
+++ Quizzes/Quiz07/quiz07.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+struct Wallet
+{
+	unsigned int bills[6];
+	unsigned int cents[4];
+};
+
+//start writing the definition below here
+
+int main()
+{
+	return 0;
+}
